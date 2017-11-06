8b229537e12479ff11c870f7a9840055e7a713a0
Daniel Chan <iam@danielchan.me>
2017-11-06 17:36:22 -0500

    Models for the objects.

---
+++ app/backend/migrations/20171106223247-create-submission.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('submissions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      plagiarism_score: {
+        type: Sequelize.INTEGER
+      },
+      student_id: {
+        type: Sequelize.STRING
+      },
+      file_hash: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('submissions');
+  }
+};
---
+++ app/backend/migrations/20171106223358-create-assignment.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('assignments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      plagiarism_threshold: {
+        type: Sequelize.INTEGER
+      },
+      folder_hash: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('assignments');
+  }
+};
