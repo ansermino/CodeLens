0b056e79cfbe96da6b3a268321807825faaf54b5
Daniel Chan <iam@danielchan.me>
2017-11-06 17:38:55 -0500

    Initial DB Connection

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const models = require('./models');
+
+models.sequelize.sync().then(() => {
+  console.log("Database connection successfully established.");
+}).catch((err) => {
+  console.log(err, "Something went wrong connecting to the database!");
+});
 
 
 
