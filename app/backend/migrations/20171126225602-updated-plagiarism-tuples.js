cbf08d311f6f709032b7202de08d55f858df54ec
Pat Gronowski <patgronowski@users.noreply.github.com>
2017-11-26 18:27:55 -0500

    Add results route and updated plagiarism tuples table

---
+++ app/backend/migrations/20171126225602-updated-plagiarism-tuples.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+      */
+      queryInterface.addColumn('plagiarism_tuples', 'assignment_id', {
+          type: Sequelize.INTEGER,
+          references: {
+              model: 'assignments',
+              key: 'id'
+          }
+      });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
--- app/backend/routes/api_assignments.js
+++ app/backend/routes/api_assignments.js
@@ -4,12 +4,33 @@ const util = require('./util');
 const models = require('../models');
 const Assignments = models.assignment;
 const Submissions = models.submission;
+const Results = models.plagiarism_tuples;
 
 router.get('/all', function(req, res) {
 	Assignments.findAll({attributes: ['id', 'title']}).then(assignments => {
 		res.json(assignments);
 	});
 });
+
+router.get('/:id/results', function(req, res) {
+	Results.findAll({
+		where: {
+			assignment_id: req.params.id,
+		},
+		attributes: ['submission_a', 'submission_b', 'plagiarism_score']
+	}).then(results => {
+
+		let compare = function(a,b) {
+			if (a.plagiarism_score < b.plagiarism_score)
+				return 1;
+			if (a.plagiarism_score > b.plagiarism_score)
+				return -1;
+			return 0;
+		}
+
+		res.json(results.sort(compare));
+	})
+})
 
 router.get('/submissions/:id', function(req, res) {
 	Submissions.findAll({
