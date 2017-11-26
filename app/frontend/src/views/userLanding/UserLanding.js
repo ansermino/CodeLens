ee5d239cb78bb5bd53d3a8c7ee204b59173522c3
David Ansermino <david.ansermino@mail.utoronto.ca>
2017-11-26 15:59:55 -0500

    Implemented assignment fetching

--- app/frontend/src/views/userLanding/UserLanding.js
+++ app/frontend/src/views/userLanding/UserLanding.js
@@ -24,18 +24,38 @@ const addAssignmentStyle = {
     margin: "3rem"
 }
 
+const getAssignments = () => new Promise((resolves, rejects) => {
+  const url = 'localhost:3000/api/getAssignments'
+  const request = new XMLHttpRequest()
+  request.open('GET', url)
+  request.onload = () => resolves(JSON.parse(request.response))
+  request.onerror = (err) => rejects(err)
+  request.send()
+})
+
 
 class UserLanding extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {assignments: []}
+  }
+  componentDidMount(){
+    getAssignments.then((data)=>{
+      console.log(data)
+      this.setState({assignments: data})
+      this.render()
+    }, (error)=>{
+      console.log(error)
+    })
+  }
   render(){
     return(
       <div style={rootDivStyle}>
-
         <div>
-
         </div>
         <div>
           <h2>Assignments</h2>
-          {samepleData.map((item, n) => <AssignmentBar title={item.title} />)}
+          {this.state.assignments.map((item, n) => <AssignmentBar title={item.title} />)}
         </div>
         <div>
         <Link to="/create/name"><FloatingActionButton style={addAssignmentStyle}>
