867cfc3c90eb0866edb26800f36559c675c8494c
David Ansermino <david.ansermino@mail.utoronto.ca>
2017-11-26 19:10:42 -0500

    Added CORS

--- app/backend/server.js
+++ app/backend/server.js
@@ -11,7 +11,11 @@ const passport = require('passport');
 // Specifically for test. Should have a check for environment variables, dev or prod.
 const testViews = require('./routes/api_test');
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({
   extended: true
 }));
