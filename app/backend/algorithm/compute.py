4562ef2bd1d7db4d8b0f95079968efd0e3f3aa91
Frank Karunaratna <frankdilank@hotmail.com>
2017-12-01 16:42:40 -0500

    Cleaned up compute.py

--- app/backend/algorithm/compute.py
+++ app/backend/algorithm/compute.py
@@ -7,19 +7,22 @@ import psycopg2
 
 def run_algo_on_dirs(dir1, dir2, starter=None):
     """
-    Runs the plagiarism checker on the files in the two directories.
+    Runs the plagiarism checker on all the files in the two directories.
     """
 
+    # Get all the files in dir1
     dir1_files = os.listdir(dir1)
     dir1_files.sort()
 
+    # Get all the files in dir2
     dir2_files = os.listdir(dir2)
     dir2_files.sort()
 
+    # Get all the starter files
     starter_files = os.listdir(starter)
     starter_files.sort()
 
-
+    # Make sure the dir's have the same number of files
     if (len(dir1_files) != len(dir2_files)):
         print("The directories must have the same number of files.")
         sys.exit()
@@ -28,9 +31,11 @@ def run_algo_on_dirs(dir1, dir2, starter=None):
         print("The directories must have the same files.")
         sys.exit()
 
+    # Connect to the db
     conn = psycopg2.connect("dbname=plagiarism user=csc301 password=team01")
     cur = conn.cursor()
 
+    # Run the algo on all the matching files in the directory
     for x in range(len(dir1_files)):
 
         if dir1_files[x] in starter_files:
