15ea0e155fa60f1ff87f58c302290cd160b3b529
Pat Gronowski <patgronowski@users.noreply.github.com>
2017-12-07 14:58:19 -0500

    Add diff route

--- app/backend/routes/api_submissions.js
+++ app/backend/routes/api_submissions.js
@@ -5,6 +5,8 @@ const models = require('../models');
 const Assignments = models.assignment;
 const Submissions = models.submission;
 const Results = models.plagiarism_tuples;
+const fs = require('fs');
+
 
 router.get('/submissions/:id', function(req, res) {
 	Submissions.findAll({
@@ -16,5 +18,57 @@ router.get('/submissions/:id', function(req, res) {
 		res.json(submissions);
 	})
 });
+
+
+router.get('/diff/:assignment_id/:submission_a/:submission_b', function(req, res) {
+	// Find the folder hash of the assignments.
+
+	diff_object = {};
+
+	// Find where the assignment is stored.
+	Assignments.findAll({
+		where: {
+			id: req.params.assignment_id,
+		},
+		attributes: ['folder_hash']
+	}).then ((folder_hash) => {
+
+		// Get the lines of submission a and submission b.
+		Results.findAll({
+			where: {
+				assignment_id: req.params.assignment_id,
+				submission_a: req.params.submission_a,
+				submission_b: req.params.submission_b,
+			},
+			attributes: ['lines_1', 'lines_2']
+		}).then((lines) => {
+
+			diff_object[lines_1] = lines.lines_1;
+			diff_object[lines_2] = lines.lines_2;
+
+			// Read the files so you can pass them in.
+			fs.readFile("PATH HERE", "utf8", function(err, data){
+				if (err) {
+					console.log(err);
+				}
+
+				// Add file contents to diff object.
+				diff_object[file_1] = data;
+
+			}).then (
+
+				fs.readFile("PATH HERE", "utf8", function(err, data){
+					if (err) {
+						console.log(err);
+					}
+
+					diff_object[file_2] = data;
+
+				});
+			);
+		});
+	});
+}
+
 
 module.exports = router;
